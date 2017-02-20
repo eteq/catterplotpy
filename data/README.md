c5d781c38e9348958a38ae0e852964cf4ac80886
Erik Tollerud <erik.tollerud@gmail.com>
2017-02-20 01:34:58 -0500

    initial import of cat data

---
+++ data/README.md
@@ -0,0 +1,6 @@
+These data files are derived from the R catterplot:
+https://github.com/Gibbsdavidl/CatterPlots. The data file ``cats.rda`` is
+copied from there, and the ``reprocess_cats.py`` script converts them into
+numpy array format.  This data file is included here so as not to require the
+user running the script, but it is retained in case ``cats.rda`` is updated
+in the future.
---
+++ data/reprocess_cats.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+
+import os
+import re
+import sys
+
+import numpy as np
+
+print ('Execturing catwrite.R script... R must be installed...')
+retcode = os.system("R --no-save < catwrite.R")
+if retcode != 0:
+    print('catwrite.R failed to execute.  Perhaps you need to install R?')
+    sys.exit(retcode)
+
+
+cats = {}
+for fn in os.listdir():
+    match = re.match(r'cat(\d?\d)', fn)
+    if match:
+        print("Processing file", fn)
+        grpi = int(match.group(1))
+
+        with open(fn) as f:
+            rows = [r.strip().split('"')[-1] for r in list(f)[1:]]
+            arr =  np.array([[float(e) for e in row.split()] for row in rows])
+            assert arr.dtype != 'O'
+            cats[grpi] = arr
+        print('Removing', fn)
+        os.remove(fn)
+
+# most of the cats should be the same shape, and begin at 216 (except for Nyan cat)
+catstack = []
+for num, cat in cats.items():
+    if cat.shape == (72, 288):
+        catstack.append(cat[:, 216:])
+
+assert len(catstack) == len(cats)-1
+catstack = np.array(catstack)
+print('Saving cat stack as cats.npy')
+np.save('cats', catstack)
